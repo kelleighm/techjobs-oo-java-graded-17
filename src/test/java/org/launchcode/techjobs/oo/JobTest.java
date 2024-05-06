69964395bd85007054377153fec29e76f8dddd22
kelleighm <kelleighmmartin@gmail.com>
2024-05-06 18:39:31 -0500

    Task 4 Completed

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,53 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        //Test for name
+        assertTrue(job.getName() instanceof String);
+        assertEquals("Product tester", job.getName());
+
+        //Test for employer
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertEquals("ACME", job.getEmployer().getValue());
+
+        //Test for location
+        assertTrue(job.getLocation() instanceof Location);
+        assertEquals("Desert", job.getLocation().getValue());
+
+        //Test for position type
+        assertTrue(job.getPositionType() instanceof  PositionType);
+        assertEquals("Quality control", job.getPositionType().getValue());
+
+        //Test for core competency
+        assertTrue(job.getCoreCompetency() instanceof  CoreCompetency);
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        //Testing that it returns 'false' - all fields are same EXCEPT for ID. Two jobs are equal if same ID
+        assertFalse(job1.equals(job2));
+    }
+
+
 }
